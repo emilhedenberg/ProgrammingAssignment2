a90de48b6a7e23bfd666d9d963adea4115a93214
Emil Hedenberg <emil.hedenberg@crepido.com>
2015-06-15 23:09:05 +0200

    Better comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create a cachable matrix, and to perform a solve inversion on such a matrix
 
-## Write a short comment describing this function
-
+## Create a new matrix that is cachable, holds a field for a solved matrix
+## Includes getters and setters for value object and solved object
 makeCacheMatrix <- function(x = matrix()) {
 	s <- NULL
 	set <- function(y) {
@@ -16,8 +15,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Perform Inversion with solve on a matrix
+## If it has been inverted, return that value from cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	s <- x$getsolve()
